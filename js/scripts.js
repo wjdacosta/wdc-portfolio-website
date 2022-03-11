f1676981d5af0df9c28a4e551836a6842ab53b5b
wjdacosta <info@willdacosta.design>
2022-03-11 13:21:38 +0000

    JavaScript Libraries & Plugins Menu Icon

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,9 @@
-/* Hello world */
-console.log("Hello world")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
